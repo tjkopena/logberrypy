c02ade4fc9d8a6b0c06bd0f506baa330f04eee5e
tjkopena <joseph.kopena@drakontas.com>
2020-06-15 06:39:35 -0400

    build: Janus is required

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 setup(
-    name            = 'LogberryPy',
-    version         = '0.0.1',
-    description     = 'Structured logging library',
-    packages        = ['logberry'],
-    python_requires = '>=3.6',
-    author          = 'Joseph Kopena',
-    author_email    = 'tjkopena@gmail.com',
+    name             = 'logberry',
+    version          = '0.2.0',
+    description      = 'Structured logging library',
+    packages         = ['logberry'],
+    python_requires  = '>=3.6',
+    author           = 'Joseph Kopena',
+    author_email     = 'tjkopena@gmail.com',
+    install_requires = ["janus"],
 )
