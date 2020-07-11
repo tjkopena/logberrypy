42c5fe3290788c2372fb2c55dc81e33a388976b1
tjkopena <joseph.kopena@drakontas.com>
2020-07-11 09:43:55 -0400

    test: basics

---
+++ tests/integration/test001-doublestart.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import logberry
+
+logberry.start()
+logberry.start()
+logberry.stop()
+
+## SPEC
+#\d{8}T\d\d:\d\d:\d\d.\d{6} DONE\s+main\s+1\s+
---
+++ tests/integration/test010-emit-no_timespec.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import logberry
+
+logberry.start(emitters=[logberry.Printer(timespec=None)])
+logberry.stop()
+
+## SPEC
+#DONE\s+main\s+1\s+
